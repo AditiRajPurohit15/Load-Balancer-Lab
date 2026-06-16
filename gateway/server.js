8a5c49e10a0be85c1cfb39989b11dbedb7a4f945
AditiRajPurohit15 <aditipurohit1505@gmail.com>
2026-06-16 12:20:28 +0530

    reverse proxy v0

--- gateway/server.js
+++ gateway/server.js
@@ -1,14 +1,23 @@
 const express = require("express");
 
 const app = express();
-
+app.use(express.json())
 const PORT = 5000;
 
 const targetServer ="http://localhost:8001"
 app.use(async(req, res) => {
 //    console.log(req.path);
 //    res.send("Gateway received request");
-    const backendResponse = await fetch(targetServer + req.path)
+    const options={
+        method: req.method,
+        headers: req.headers
+    }
+    if (req.body) {
+    options.body = JSON.stringify(req.body);
+    }
+    const backendResponse = await fetch(targetServer + req.originalUrl, options)
+    // console.log(backendResponse)
+    res.status(backendResponse.status);
     const data = await backendResponse.text();
     res.send(data);
 });
