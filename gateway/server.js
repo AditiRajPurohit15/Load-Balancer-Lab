cb54496e513f7149c00b9e56ea10bf296c42aed6
AditiRajPurohit15 <aditipurohit1505@gmail.com>
2026-06-17 16:49:02 +0530

    Round Robin Load Balancer V1 Complete

--- gateway/server.js
+++ gateway/server.js
@@ -4,26 +4,52 @@ const app = express();
 app.use(express.json())
 const PORT = 5000;
 
-const targetServer ="http://localhost:8001"
+const servers = [
+    "http://localhost:8001",
+    "http://localhost:8002"
+];
+
+let currentServer=0;
+
+
 app.use(async(req, res) => {
 //    console.log(req.path);
 //    res.send("Gateway received request");
     try {
+
         const options={
         method: req.method,
         headers: req.headers
-    }
-    if (req.body) {
-    options.body = JSON.stringify(req.body);
-    }
-    const backendResponse = await fetch(targetServer + req.originalUrl, options)
-    // console.log(backendResponse)
-    res.status(backendResponse.status);
-    for(const [key,value] of backendResponse.headers){
+        }
+
+        if (req.body) {
+        options.body = JSON.stringify(req.body);
+        }
+
+            if (req.originalUrl !== "/") {
+    return res.status(404).send("Ignored");
+}
+
+        const targetServer = servers[currentServer % servers.length];
+        currentServer++;
+        
+        console.log(req.originalUrl);
+        console.log(targetServer);
+
+    
+
+        const backendResponse = await fetch(targetServer + req.originalUrl, options)
+        // console.log(backendResponse)
+        res.status(backendResponse.status);
+
+        for(const [key,value] of backendResponse.headers){
         res.set(key,value)
-    }
-    const data = await backendResponse.text();
-    res.send(data);
+        }
+
+        const data = await backendResponse.text();
+
+        res.send(data);
+
     } catch (error) {
         console.error(error);
         return res.status(502).json({
